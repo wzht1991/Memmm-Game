c0f6367a73dd441d39c61a9b7d911147ddcf6c1c
wzht1991 <506486189@qq.com>
2019-01-18 12:39:51 +1000

    almost finish (why click the button it will call event two times?)

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ var game={
 // new game section
 //  new level 1 game
 function level_1(){ 
-       console.log("Level 1")
        var New_Game = document.querySelector('.game-stats__button');
        var game_board = document.querySelector('.game-board');
        var time_bar= document.querySelector(".game-timer__bar");
@@ -36,7 +35,7 @@ function level_1(){
        //change html(innerhtml 只有在这个function 有效)
       // game_board.innerHTML='<div class="card css3" data-tech="css3"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div> </div><div class="card html5" data-tech="html5"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div><div class="card css3" data-tech="css3"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div><div class="card html5" data-tech="html5"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div></div>'
       // 这是一个记录等待匹配元素的数组 后面需要用来匹配的
-       var used=[]
+       var used=[];
      // lvel 1专用 把匹配成功的元素放在这里从而防止4张牌都一样
        var usedEle;
        // level just have 4 cards 一定要确认每张卡都是一对的
@@ -46,7 +45,6 @@ function level_1(){
         var index=random(0,1);
         while(Class_Name[index]==usedEle){
             index=random(0,1);
-            console.log("111");
         }
          //如果还剩下4-i(还剩下多少空位)=used 的长度时候触发 因为这说明后面的几个位置得全部给未匹配成功的元素
          if((4-i)==used.length){
@@ -79,8 +77,113 @@ function level_1(){
        flip(); 
        time(); 
 }
+function Level2(){
+    var New_Game = document.querySelector('.game-stats__button');
+       var game_board = document.querySelector('.game-board');
+       var time_bar= document.querySelector(".game-timer__bar");
+       //create a array of class name
+       var Class_Name=["css3","html5","js","react","nodejs","sass","linkedin","heroku","github","aws"];  
+       var score=document.querySelector(".game-stats__score--value");
+       score.innerHTML=game.score;
+      //重新开始的时候让他显示60秒 合满血状态
+       time_bar.innerHTML="60s";
+       time_bar.style.width="100%";
+        //  把网格分成了4分 4个fr 这样子每一行有四个卡牌 网格系统 disply： grid 才能用
+       game_board.setAttribute('style','grid-template-columns: 1fr 1fr 1fr 1fr;');
+
+       //  change button name
+       New_Game.innerHTML="End Game";
+       var used=[];
+       var usedEle;
+       for(var i=0;i<16;i++){
+        // 随机0到1 包括1的整数 level 1 专用要是匹配出来的是用过的元素就继续匹配
+        var index=random(0,9);
+        while(Class_Name[index]==usedEle){
+            index=random(0,9);
+        }
+         //如果还剩下16-i(还剩下多少空位)=used 的长度时候触发 因为这说明后面的几个位置得全部给未匹配成功的元素
+         if((16-i)==used.length){
+            var used_index=random(0,(used.length-1));
+            game_board.innerHTML+='<div class="card'+' '+used[used_index]+'" data-tech="'+used[used_index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+            //删除掉里面的元素啊
+            used.splice(used.indexOf(used[used_index]),1);
+            }else{
+         //如果这里就用+=会把那个前面介绍也弄进去的
+         if(i==0){
+            game_board.innerHTML='<div class="card'+' '+Class_Name[index]+'" data-tech="'+Class_Name[index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+                 // 把使用过的元素放入数组
+                 used.push(Class_Name[index]); 
+         }else{
+                game_board.innerHTML+='<div class="card'+' '+Class_Name[index]+'" data-tech="'+Class_Name[index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+            // 判断used 数组里面是否存在这个元素 不存在就-1 确保元素的唯一性
+             if(used.indexOf(Class_Name[index])==-1){
+                // 把使用过的元素放入数组
+                used.push(Class_Name[index]);
+            }else{
+              // 第一个指删除元素的index 第二个指删除的数目 匹配成功后删除
+              used.splice(used.indexOf(Class_Name[index]),1);
+               usedEle=Class_Name[index];//level 1专用 把匹配成功的元素放在这里
+           }
+        } 
+    } 
+ }     
+       flip();
+       time(); 
+}
+
+// level 3
+function Level3(){
+    var New_Game = document.querySelector('.game-stats__button');
+     var game_board = document.querySelector('.game-board');
+     var time_bar= document.querySelector(".game-timer__bar");
+     var Class_Name=["css3","html5","js","react","nodejs","sass","linkedin","heroku","github","aws"];  
+     var score=document.querySelector(".game-stats__score--value");
+     score.innerHTML=game.score;
+     time_bar.innerHTML="60s";
+     time_bar.style.width="100%";
+     game_board.setAttribute('style','grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;');
+     New_Game.innerHTML="End Game";
+     var used=[];
+     var usedEle;
+     for(var i=0;i<36;i++){
+        // 随机0到1 包括1的整数 level 1 专用要是匹配出来的是用过的元素就继续匹配
+        var index=random(0,9);
+        while(Class_Name[index]==usedEle){
+            index=random(0,9);
+        }
+         //如果还剩下4-i(还剩下多少空位)=used 的长度时候触发 因为这说明后面的几个位置得全部给未匹配成功的元素
+         if((36-i)==used.length){
+            var used_index=random(0,(used.length-1));
+            game_board.innerHTML+='<div class="card'+' '+used[used_index]+'" data-tech="'+used[used_index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+            //删除掉里面的元素啊
+            used.splice(used.indexOf(used[used_index]),1);
+            }else{
+         //如果这里就用+=会把那个前面介绍也弄进去的
+         if(i==0){
+            game_board.innerHTML='<div class="card'+' '+Class_Name[index]+'" data-tech="'+Class_Name[index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+                 // 把使用过的元素放入数组
+                 used.push(Class_Name[index]);
+         }else{
+            game_board.innerHTML+='<div class="card'+' '+Class_Name[index]+'" data-tech="'+Class_Name[index]+'"><div class="card__face card__face--front"></div><div class="card__face card__face--back"></div></div>'; 
+             // 判断used 数组里面是否存在这个元素 不存在就-1 确保元素的唯一性
+             if(used.indexOf(Class_Name[index])==-1){
+                // 把使用过的元素放入数组
+                used.push(Class_Name[index]);
+            }else{
+              // 第一个指删除元素的index 第二个指删除的数目 匹配成功后删除
+              used.splice(used.indexOf(Class_Name[index]),1);
+               usedEle=Class_Name[index];//level 1专用 把匹配成功的元素放在这里
+           }
+        } 
+    } 
+ }
+        // The class of innerHTML's tag(such as card css3 ) just effect in this section so I just can use flip function in this function
+       // 这个界面是点击后才会出现的 所以里面的类名也应该在这里
+       flip(); 
+       time(); 
+     
+}
 function jump1(){
-       console.log("jump 1")
        var New_Game = document.querySelector('.game-stats__button');
     //    var game_board = document.querySelector('.game-board');
     //    //create a array of class name
@@ -166,16 +269,10 @@ function flip(){
                     card_f[Card1_Index].style.webkitbackfaceVisibility="hidden";
                 },500); 
                 }
-                // 就是匹配成功了+2分
+                // 就是匹配成功了+2分(满分后时间停止而且过关 在 time() 方法里)
                 else{
                     game.score+=2
                     score_display.innerHTML=game.score;
-                    if(game.level==1){
-                    if(game.score==4){
-                        game.level=2;
-                        jump2();
-                    }
-                }
                 }
               }
           })
@@ -205,44 +302,62 @@ function time(){
             alert("Congratulations! your score is "+game.score);  
             game.display_time=60;
             game.score=0;
+            // 无论多少关都要回到第一关
+            game.level=1;
             start.innerHTML="Start Game";
-            // level 1
-            if(game.level==1){
             // 因为一开始整个function 是执行所有代码在执行计时 所以按钮被赋予了start.addEventListener('click',b); 
             // 所以计时结束的时候要remove 的是('click',b)而不是('click',level_1);
             start.removeEventListener('click',b);
             start.addEventListener('click',level_1);
-            }
 
-        } 
+        }else{
+          if(game.level==1){
+            // 分数到了4分 过关后 时间停止进入下一关
+            if(game.score==4){
+               clearInterval(interval);  
+               game.level=2;
+               game.display_time=60;
+               Level2();
+            } 
+         }else if(game.level==2){
+            if(game.score==20){ 
+            clearInterval(interval);  
+            game.level=3;
+            game.display_time=60;
+            Level3();
+            }
+          }else if(game.level==3){
+              if(game.score==56){
+                clearInterval(interval);
+                alert("congratulation!! you win");
+              }
+          }
+        }
   },1000)
 
     //  为了方便 remove 因为 removeeventlistener 不能用整个函数
        var b=function(){
+         start.removeEventListener('click',b); 
          Back(interval,start);
-         start.removeEventListener('click',b);
-         if(game.level==1){
-            start.addEventListener('click',level_1);  
-         }
+        // 全部都需要从第一关开始
+         start.addEventListener('click',level_1);  
         }
         // 随时停止 
-        if(game.level==1){
             start.removeEventListener('click',level_1);
-            // 这个东西会马上触发的 要是 直接（）=》{} 这样子就不会
+         // 这个东西会马上触发的 要是 直接（）=》{} 这样子就不会
             // start.addEventListener('click',Back(interval,start));  
             // 这样就没事了 正常了
             start.addEventListener('click',b); 
-            // 这个不能放下面 因为代码一开始就会被执行 所以就会add 了之后 马上remove了 所以只能放在function 里面
+         // 这个不能放下面 因为代码一开始就会被执行 所以就会add 了之后 马上remove了 所以只能放在function 里面
             // start.removeEventListener('click',b);
-        }
-
 }
 
 function Back(interval,start){
-    console.log("sdfsd");
     clearInterval(interval);
     alert("Congratulations! your score is "+game.score); 
     game.display_time=60;
     game.score=0;
+    game.level=1;
     start.innerHTML="Start Game";
 }
+// 第二关为何随时停止时候还会出现两次弹窗 back function 的弹窗
